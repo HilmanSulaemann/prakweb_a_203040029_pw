6c31068166c5f9284597055087884554166c91b2
hilmansulaeman <hilmansulaeman0504@gmail.com>
2022-10-03 14:05:08 +0700

    Mengubah Background

--- pw/index.php
+++ pw/index.php
@@ -25,6 +25,7 @@ $buku = $rows;
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Buku</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
